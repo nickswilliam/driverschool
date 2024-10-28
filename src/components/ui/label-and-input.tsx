9f0d5fcf858f1a2b7403a53314d40f7d7272ff3d
Nicolás Guillermo Sánchez <109560335+nickswilliam@users.noreply.github.com>
2024-10-27 23:04:59 -0300

    Update label-and-input.tsx

--- src/components/ui/label-and-input.tsx
+++ src/components/ui/label-and-input.tsx
@@ -7,18 +7,17 @@ interface ILabelAndInput {
   title: string;
   value: string | number;
   type: "text"| "number";
-  disabled?: unknown,
   id: number;
 }
 
-export const LabelAndInput = ({ disabled, title, value, id, type }: ILabelAndInput) => {
+export const LabelAndInput = ({  title, value, id, type }: ILabelAndInput) => {
   return !value ? null : (
     <div className="flex flex-col gap-2">
       <CustomLabel title={title} />
 
       {/* container for input to validate roles */}
       <div className="flex w-full gap-2">
-        <CustomInput disabled value={value} type={type}/>
+        <CustomInput value={value} type={type}/>
         <EditDashboard id={id}/>
       </div>
     </div>
