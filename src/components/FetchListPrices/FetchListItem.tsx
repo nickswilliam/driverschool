58907b9317f555ff8f6bf5d906d2112810afd08c
Nicolás Guillermo Sánchez <109560335+nickswilliam@users.noreply.github.com>
2024-10-27 23:06:51 -0300

    Update FetchListItem.tsx

--- src/components/FetchListPrices/FetchListItem.tsx
+++ src/components/FetchListPrices/FetchListItem.tsx
@@ -10,9 +10,9 @@ interface IFetchListItem {
 export const FetchListItem = ({ id, courseTitle, href, idLink }: IFetchListItem) => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4 border rounded-md py-2 px-3 shadow-md">
-      <LabelAndInput disabled={true} title="Identificador" value={id} id={idLink} type="number"/>
-      <LabelAndInput disabled={true} title="Curso" value={courseTitle} id={idLink} type="text"/>
-      <LabelAndInput disabled={true} title="Link de Pago:" value={href} id={idLink} type="text"/>
+      <LabelAndInput title="Identificador" value={id} id={idLink} type="number"/>
+      <LabelAndInput title="Curso" value={courseTitle} id={idLink} type="text"/>
+      <LabelAndInput title="Link de Pago:" value={href} id={idLink} type="text"/>
     </div>
   );
 };
